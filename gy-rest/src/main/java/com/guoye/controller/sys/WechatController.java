7c8d57b85c658cd45f6cefebd82752cb6588dbc9
zhangxulei <2435226189@qq.com>
2020-02-15 10:47:38 +0800

    215

--- gy-rest/src/main/java/com/guoye/controller/sys/WechatController.java
+++ gy-rest/src/main/java/com/guoye/controller/sys/WechatController.java
@@ -175,43 +175,43 @@ public class WechatController extends BizAction {
      * @param iv
      * @return
      */
-    public JSONObject decryptUserInfo(String encryptedData, String sessionkey, String iv) {
-        encryptedData = encryptedData.replaceAll(" ", "+");
-        // 被加密的数据
-        byte[] dataByte = org.bouncycastle.util.encoders.Base64.decode(encryptedData);
-        // 加密秘钥
-        byte[] keyByte = org.bouncycastle.util.encoders.Base64.decode(sessionkey);
-        // 偏移量
-        byte[] ivByte = Base64.decode(iv);
-        try {
-            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
-            int base = 16;
-            if (keyByte.length % base != 0) {
-                int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
-                byte[] temp = new byte[groups * base];
-                Arrays.fill(temp, (byte) 0);
-                System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
-                keyByte = temp;
-            }
-            // 初始化
-            Security.addProvider(new BouncyCastleProvider());
-            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
-            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
-            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
-            parameters.init(new IvParameterSpec(ivByte));
-            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);
-            byte[] resultByte = cipher.doFinal(dataByte);
-            if (null != resultByte && resultByte.length > 0) {
-                String result = new String(resultByte, StandardCharsets.UTF_8);
-                return JSON.parseObject(result);
-            }
-        } catch (Exception e) {
-            logger.error("获取unionId异常", e);
-            throw new RuntimeException("获取unionId异常", e);
-        }
-        throw new RuntimeException("获取联合登陆用户信息失败");
-
-    }
+//    public JSONObject decryptUserInfo(String encryptedData, String sessionkey, String iv) {
+//        encryptedData = encryptedData.replaceAll(" ", "+");
+//        // 被加密的数据
+//        byte[] dataByte = org.bouncycastle.util.encoders.Base64.decode(encryptedData);
+//        // 加密秘钥
+//        byte[] keyByte = org.bouncycastle.util.encoders.Base64.decode(sessionkey);
+//        // 偏移量
+//        byte[] ivByte = Base64.decode(iv);
+//        try {
+//            // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
+//            int base = 16;
+//            if (keyByte.length % base != 0) {
+//                int groups = keyByte.length / base + (keyByte.length % base != 0 ? 1 : 0);
+//                byte[] temp = new byte[groups * base];
+//                Arrays.fill(temp, (byte) 0);
+//                System.arraycopy(keyByte, 0, temp, 0, keyByte.length);
+//                keyByte = temp;
+//            }
+//            // 初始化
+//            Security.addProvider(new BouncyCastleProvider());
+//            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS7Padding", "BC");
+//            SecretKeySpec spec = new SecretKeySpec(keyByte, "AES");
+//            AlgorithmParameters parameters = AlgorithmParameters.getInstance("AES");
+//            parameters.init(new IvParameterSpec(ivByte));
+//            cipher.init(Cipher.DECRYPT_MODE, spec, parameters);
+//            byte[] resultByte = cipher.doFinal(dataByte);
+//            if (null != resultByte && resultByte.length > 0) {
+//                String result = new String(resultByte, StandardCharsets.UTF_8);
+//                return JSON.parseObject(result);
+//            }
+//        } catch (Exception e) {
+//            logger.error("获取unionId异常", e);
+//            throw new RuntimeException("获取unionId异常", e);
+//        }
+//        throw new RuntimeException("获取联合登陆用户信息失败");
+//
+//    }
 
 
 }

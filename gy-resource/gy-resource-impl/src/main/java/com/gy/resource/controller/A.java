9a45a1371b2cffae2c9e424ffd63a5f417199d4f
zhuxiankun <862446268@qq.com>
2020-02-19 17:55:02 +0800

    Merge branch 'master' of http://116.62.227.95/guoye/gy-rest
    # Conflicts:
    #	gy-resource/gy-resource-impl/src/main/java/com/gy/resource/controller/rest/MyFollowController.java

--- gy-resource/gy-resource-impl/src/main/java/com/gy/resource/controller/A.java
+++ gy-resource/gy-resource-impl/src/main/java/com/gy/resource/controller/A.java
@@ -1,9 +1,25 @@
 package com.gy.resource.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 /**
  * @author: gaolanyu
  * @date: 2020-02-14
  * @remark:
  */
 public class A {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList();
+        Set<String> set=new HashSet<String>();
+        list.add("a");
+        list.add("a");
+        list.add("b");
+        for(String str:list){
+            set.add(str);
+        }
+        System.out.println("----------"+set.toString());
+    }
 }
